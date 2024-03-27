981d20aa569f4aae97b1c760c457427852500566
gitaroktato <oresztesz_margaritisz@epam.com>
2024-03-27 09:09:31 +0100

    feat: Working with numpy arrays

--- src/queue.py
+++ src/queue.py
@@ -7,8 +7,7 @@ class Queue:
         # arrival times - sorted
         self._arrival_times = Queue._inter_arrival_time_to_arrival_time(inter_arrival_times)
         self._execution_times = execution_times
-        self._departure_times = []
-        self._wait_times = []
+        self._departure_times = np.empty_like(inter_arrival_times)
         self._executor_at = 0
 
     def process(self):
@@ -22,10 +21,10 @@ class Queue:
             start_at = max(self._executor_at, arrive_at)
             processed_at = start_at + self._execution_times[index]
             self._executor_at = processed_at
-            self._departure_times.append(processed_at)
+            self._departure_times[index] = processed_at
 
     def _process_wait_times(self):
-        self._wait_times = np.array(self._departure_times) - np.array(self._arrival_times)
+        self._wait_times = self._departure_times - self._arrival_times
 
     def _process_length(self):
         last_departure_at = self._departure_times[-1] + 1
