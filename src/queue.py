c7b7e64d69c7519df9e88d8b861253b74bf3934d
gitaroktato <oresztesz_margaritisz@epam.com>
2024-03-27 08:44:27 +0100

    feat: Calculating wait times

--- src/queue.py
+++ src/queue.py
@@ -8,30 +8,40 @@ class Queue:
         self._arrival_times = Queue._inter_arrival_time_to_arrival_time(inter_arrival_times)
         self._execution_times = execution_times
         self._departure_times = []
+        self._wait_times = []
         self._executor_at = 0
 
     def process(self):
         self._process_departure_times()
         self._process_wait_times()
+        self._process_length()
 
     def _process_departure_times(self):
         for index, arrive_at in enumerate(self._arrival_times):
+            # we can start only if previous execution is finished
             start_at = max(self._executor_at, arrive_at)
             processed_at = start_at + self._execution_times[index]
             self._executor_at = processed_at
             self._departure_times.append(processed_at)
 
     def _process_wait_times(self):
+        self._wait_times = np.array(self._departure_times) - np.array(self._arrival_times)
+
+    def _process_length(self):
         last_departure_at = self._departure_times[-1] + 1
-        self._wait_times = np.zeros(last_departure_at)
+        self._length = np.zeros(last_departure_at)
         for arrive_at in self._arrival_times:
-            self._wait_times[arrive_at:] += 1
+            self._length[arrive_at:] += 1
         for depart_at in self._departure_times:
-            self._wait_times[depart_at:] -= 1
+            self._length[depart_at:] -= 1
 
     @property
     def departure_times(self):
         return self._departure_times
+
+    @property
+    def length(self):
+        return self._length
 
     @property
     def wait_times(self):
--- test/queue_test.py
+++ test/queue_test.py
@@ -1,29 +1,46 @@
-import numpy
 import numpy as np
-import pytest
-from numpy import ndarray
 
 from src.queue import Queue
 
 
 class TestQueue:
 
-    def test_to_departure_times(self):
+    def test_departure_times(self):
         inter_arrival_time = [1, 1, 1, 1]
         execution_time = [1, 1, 1, 1]
         queue = Queue(inter_arrival_time, execution_time)
         queue.process()
         assert queue.departure_times == [2, 3, 4, 5]
 
-    def test_to_departure_times_with_saturation(self):
+    def test_departure_times_with_saturation(self):
         inter_arrival_time = np.array([1, 1, 1, 1])
         execution_time = np.array([2, 2, 2, 2])
         queue = Queue(inter_arrival_time, execution_time)
         queue.process()
         assert queue.departure_times == [3, 5, 7, 9]
-        assert queue.wait_times.tolist() == [0, 1, 2, 2, 3, 2, 2, 1, 1, 0]
 
-    def test_inter_arrival_times(self):
+    def test_queue_size_with_saturation(self):
+        inter_arrival_time = np.array([1, 1, 1, 1])
+        execution_time = np.array([2, 2, 2, 2])
+        queue = Queue(inter_arrival_time, execution_time)
+        queue.process()
+        assert queue.length.tolist() == [0, 1, 2, 2, 3, 2, 2, 1, 1, 0]
+
+    def test_wait_times(self):
+        inter_arrival_time = [1, 1, 1, 1]
+        execution_time = [1, 1, 1, 1]
+        queue = Queue(inter_arrival_time, execution_time)
+        queue.process()
+        assert queue.wait_times.tolist() == [1, 1, 1, 1]
+
+    def test_wait_times_with_saturation(self):
+        inter_arrival_time = np.array([1, 1, 1, 1])
+        execution_time = np.array([2, 2, 2, 2])
+        queue = Queue(inter_arrival_time, execution_time)
+        queue.process()
+        assert queue.wait_times.tolist() == [2, 3, 4, 5]
+
+    def test_converting_from_inter_arrival_times_to_arrival_times(self):
         assert Queue._inter_arrival_time_to_arrival_time([1, 1, 1, 1]) == [1, 2, 3, 4]
         assert Queue._inter_arrival_time_to_arrival_time([1, 2, 3, 4]) == [1, 3, 6, 10]
 
